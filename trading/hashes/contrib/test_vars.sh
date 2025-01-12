07103149c63947ef09a9b3cb9e746a509fa8b44a
saogao <187827554+saogao@users.noreply.github.com>
2025-01-12 09:16:45 +0000

    feat: add mascot to README

---
+++ trading/hashes/contrib/test_vars.sh
@@ -0,0 +1,14 @@
+# No shebang, this file should not be executed.
+# shellcheck disable=SC2148
+#
+# disable verify unused vars, despite the fact that they are used when sourced
+# shellcheck disable=SC2034
+
+# Test all these features with "std" enabled.
+FEATURES_WITH_STD="bitcoin-io serde small-hash"
+
+# Test all these features without "std" enabled.
+FEATURES_WITHOUT_STD="alloc bitcoin-io serde small-hash"
+
+# Run these examples.
+EXAMPLES=""
